3d35894bd9a0370eab13dc78676568980dc49d7f
Mkhuleko05 <128092865+Mkhuleko05@users.noreply.github.com>
2023-09-28 12:20:52 +0200

    first commit

---
+++ Dynamic-web-apps/Script.js
@@ -0,0 +1,42 @@
+// scripts.js
+
+const form = document.querySelector("[data-form]");
+const result = document.querySelector("[data-result]");
+
+form.addEventListener("submit"), (event) => {
+  event.preventDefault();
+  const entries = new FormData(event.target);
+  const { dividend, divider } = Object.fromEntries(entries);
+}
+  
+  if (!dividend || !divider) {
+    throw new error("Division not performed. Both values are required in inputs. Try again.");
+    
+  }
+
+  // Scenario: Providing anything that is not a number should crash the program
+  if (isNaN(dividend) || isNaN(divider)) {
+    document.body.innerHTML = "<h1>Something critical went wrong. Please reload the page</h1>";
+    console.error("Invalid input. Please provide valid numbers.");
+    return;
+  }
+
+  const dividendNum = parseFloat(dividend);
+  const dividerNum = parseFloat(divider);
+
+  // Scenario: An invalid division should log an error in the console
+  if (dividerNum < 0 ) {
+    result.innerText = "Division not performed. Invalid number provided. Try again.";
+    console.error("Please provide valid numbers.");
+    return;
+  }
+  
+  // Quotient is a result obtained by dividing one quantity by another
+  const quotient = dividendNum / dividerNum;
+  const isWholeNumber = Number.isInteger(quotient);
+
+  // Scenario: Dividing numbers result in a decimal number
+  if (!isWholeNumber) {
+    result.innerText = Math.floor(quotient);
+    return;
+  }
